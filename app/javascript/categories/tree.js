3fd30c48642799835986931500f8d0dec198bf90
Jared Cosulich <jared.cosulich@gmail.com>
2020-03-08 20:18:44 -0400

    selecting questions for survey

--- app/javascript/categories/tree.js
+++ app/javascript/categories/tree.js
@@ -1,8 +1,24 @@
-setTimeout(function() {
+$( document ).on('turbolinks:load', function() {
   $('.category-selector a').click(function(event) {
     event.preventDefault();
-    const {categoryId} = event.currentTarget.dataset;
+    const categoryAnchor = event.currentTarget;
+    const {categoryId} = categoryAnchor.dataset;
     $('.category-selector-target').val(categoryId);
+
+    const category = $(categoryAnchor).closest('.category');
+    category.find('.question').each(function(index, questionElement) {
+      const {questionId} = questionElement.dataset;
+      $('#survey_question_ids_' + questionId).prop( "checked", true );
+
+    })
+
     return false;
   });
-}, 1000);
+
+  $('.question-selector a').click(function(event) {
+    event.preventDefault();
+    const {questionId} = event.currentTarget.dataset;
+    $('#survey_question_ids_' + questionId).prop( "checked", true );
+    return false;
+  });
+});
