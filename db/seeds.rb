3d29d16a4e8718ea5aef9715a10216548a3005f6
Jared Cosulich <jared.cosulich@gmail.com>
2020-03-31 13:37:06 -0400

    removing EdContext from admin index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,37 +6,39 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# r1 = Category.create(name: "Root 1")
-# r2 = Category.create(name: "Root 2")
-# r3 = Category.create(name: "Root 3")
-#
-# r1a = r1.child_categories.create(name: "R1A")
-# r1b = r1.child_categories.create(name: "R1B")
-# r2a = r2.child_categories.create(name: "R2A")
-#
-# r2aa = r2a.child_categories.create(name: "R2AA")
-#
-#
-# q1a = Question.create(
-#   text: "What is the answer to this question?",
-#   option1: "Option 1",
-#   option2: "Option 2",
-#   option3: "Option 3",
-#   option4: "Option 4",
-#   option5: "Option 5",
-#   category: r1a
-# )
-#
-# q2aa = Question.create(
-#   text: "What is the answer to this other question?",
-#   option1: "Option 1",
-#   option2: "Option 2",
-#   option3: "Option 3",
-#   option4: "Option 4",
-#   option5: "Option 5",
-#   category: r2aa
-# )
-#
-# survey = Survey.create!(name: "Survey 1")
-# survey.survey_questions.create!(question: q1a)
-# survey.survey_questions.create!(question: q2aa)
+if Rails.env.development?
+  r1 = Category.create(name: "Root 1")
+  r2 = Category.create(name: "Root 2")
+  r3 = Category.create(name: "Root 3")
+
+  r1a = r1.child_categories.create(name: "R1A")
+  r1b = r1.child_categories.create(name: "R1B")
+  r2a = r2.child_categories.create(name: "R2A")
+
+  r2aa = r2a.child_categories.create(name: "R2AA")
+
+
+  q1a = Question.create(
+    text: "What is the answer to this question?",
+    option1: "Option 1",
+    option2: "Option 2",
+    option3: "Option 3",
+    option4: "Option 4",
+    option5: "Option 5",
+    category: r1a
+  )
+
+  q2aa = Question.create(
+    text: "What is the answer to this other question?",
+    option1: "Option 1",
+    option2: "Option 2",
+    option3: "Option 3",
+    option4: "Option 4",
+    option5: "Option 5",
+    category: r2aa
+  )
+
+  survey = Survey.create!(name: "Survey 1")
+  survey.survey_questions.create!(question: q1a)
+  survey.survey_questions.create!(question: q2aa)
+end
