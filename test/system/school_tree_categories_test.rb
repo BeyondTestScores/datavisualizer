48e0415dc96cc023efe45d639a118284e8275b2f
Jared Cosulich <jared.cosulich@gmail.com>
2020-04-26 09:35:54 -0400

    fixing fixtures

--- test/system/school_tree_categories_test.rb
+++ test/system/school_tree_categories_test.rb
@@ -1,6 +1,6 @@
 require "application_system_test_case"
 
-class SchoolCategoriesTest < ApplicationSystemTestCase
+class SchoolTreeCategoriesTest < ApplicationSystemTestCase
   setup do
     @school_tree_category = school_tree_categories(:one)
   end
@@ -8,13 +8,16 @@ class SchoolCategoriesTest < ApplicationSystemTestCase
   test "school categories are automatically created with no nonlikert when adding an administrative measure" do
     visit_admin admin_root_path
 
-    click_on "+ Create New Administrative Measure"
+    click_on trees(:one)
+
+    click_on categories(:one)
+
+    click_on "+ Add An Administrative Measure To This Category"
 
     assert_text 'Administrative Measure'
 
     measure_name = 'An Administrative Measure'
     fill_in 'Name', with: measure_name
-    click_text categories(:one).name
 
     click_on 'Create'
 
@@ -26,7 +29,9 @@ class SchoolCategoriesTest < ApplicationSystemTestCase
   test "updating a school category" do
     visit_admin admin_root_path
 
-    administrative_measure = categories(:administrative_measure)
+    click_on trees(:one)
+
+    administrative_measure = tree_categories(:administrative_measure)
     click_text administrative_measure
 
     click_on 'edit', match: :first
@@ -37,6 +42,6 @@ class SchoolCategoriesTest < ApplicationSystemTestCase
     assert_text "#{school_tree_categories(:administrative_measure).name(:school)} was successfully updated"
 
     assert_text "#{school_tree_categories(:administrative_measure).name(:school)}: 3"
-    assert_text administrative_measure.parent_category
+    assert_text administrative_measure.parent_tree_category
   end
 end
