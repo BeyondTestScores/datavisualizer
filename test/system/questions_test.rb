17b1d69fbc23e04f41b9697a18212e96098459d6
Jared Cosulich <jared.cosulich@gmail.com>
2020-04-26 13:05:37 -0400

    fixing question system tests

--- test/system/questions_test.rb
+++ test/system/questions_test.rb
@@ -4,7 +4,10 @@ class QuestionsTest < ApplicationSystemTestCase
   test "creating a question" do
     visit_admin admin_root_path
 
-    click_on "+ Create New Question"
+    click_on trees(:one)
+    click_on categories(:four)
+
+    click_on "+ Add A Question To This Category"
 
     fill_in "Text", with: "What is this question?"
     fill_in "Option1", with: "An Option 1"
@@ -12,7 +15,6 @@ class QuestionsTest < ApplicationSystemTestCase
     fill_in "Option3", with: "An Option 3"
     fill_in "Option4", with: "An Option 4"
     fill_in "Option5", with: "An Option 5"
-    click_text "Category Four"
 
     click_on "Create"
 
@@ -23,13 +25,13 @@ class QuestionsTest < ApplicationSystemTestCase
   test "deleting a question" do
     requests = []
 
-    question = questions(:two)
+    tcq = tree_category_questions(:two)
 
-    question.survey_questions.each do |survey_question|
-      survey = survey_question.survey
+    tcq.school_tree_category_questions.each do |stcq|
+      survey = stcq.survey
       requests << survey_monkey_mock(
         method: :delete,
-        url: "surveys/#{survey.survey_monkey_id}/pages/#{survey_question.survey_monkey_page_id}/questions/#{survey_question.survey_monkey_id}"
+        url: "surveys/#{survey.survey_monkey_id}/pages/#{stcq.survey_monkey_page_id}/questions/#{stcq.survey_monkey_id}"
       )
 
       requests << survey_monkey_mock(
@@ -38,9 +40,9 @@ class QuestionsTest < ApplicationSystemTestCase
         responses: [
           details(
             survey: survey,
-            survey_questions: survey.survey_questions - [survey_question],
+            survey_questions: survey.school_tree_category_questions - [stcq],
             pages: [
-              {"id": survey_question.survey_monkey_page_id, "title": survey_question.question.category.name}
+              {"id": stcq.survey_monkey_page_id, "title": stcq.category.name}
             ]
           )
         ]
@@ -48,20 +50,22 @@ class QuestionsTest < ApplicationSystemTestCase
 
       requests << survey_monkey_mock(
         method: :delete,
-        url: "surveys/#{survey.survey_monkey_id}/pages/#{survey_question.survey_monkey_page_id}"
+        url: "surveys/#{survey.survey_monkey_id}/pages/#{stcq.survey_monkey_page_id}"
       )
     end
 
     visit_admin admin_root_path
 
-    click_on question.text
+    click_on trees(:one)
+
+    click_on tcq
 
     page.accept_confirm do
       click_on "Delete Question", match: :first
     end
 
     assert_text "Question was successfully destroyed"
-    assert_no_text question.text
+    assert_no_text tcq.question.text
 
     assert_requests requests
   end
