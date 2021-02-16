4c49541faac48b0cea7ca625b30592c8c08ba8f4
Jared Cosulich <jared.cosulich@gmail.com>
2021-02-16 17:11:50 -0500

    trying to get surveymonkey webhook working

---
+++ app/controllers/survey_responses_controller.rb
@@ -0,0 +1,21 @@
+class SurveyResponsesController < Admin::AdminController
+  skip_forgery_protection
+
+  def create    
+    puts ""
+    puts ""
+    puts ""
+    puts ""
+    puts params.inspect
+    puts ""
+    puts ""
+    puts ""
+    puts ""
+    if request.get?
+      render status: 200 and return 
+    end
+
+    render status: 200
+  end
+
+end
