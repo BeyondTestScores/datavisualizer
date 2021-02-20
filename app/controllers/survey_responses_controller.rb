c5efc300efdee3717f5917b84caabf1399c56921
Jared Cosulich <jared.cosulich@gmail.com>
2021-02-19 21:28:41 -0500

    working on webhook

--- app/controllers/survey_responses_controller.rb
+++ app/controllers/survey_responses_controller.rb
@@ -9,25 +9,7 @@ class SurveyResponsesController < ApplicationController
 
     info = JSON.parse(request.body.read.to_s)
 
-    puts ""
-    puts ""
-    pp info
-    puts ""
-    puts ""
-    pp info["resources"]
-    puts ""
-    puts ""
-    pp info["resources"]["survey_id"]
-    puts ""
-    puts ""
-    pp info["resources"]["respondent_id"]
-    puts ""
-    puts ""
-    pp info["object_id"]
-    puts ""
-    puts ""
-
-    survey = Survey.find_by_survey_monkey_id[info["resources"]["survey_id"]]
+    survey = Survey.find_by_survey_monkey_id(info["resources"]["survey_id"])
     survey.create_survey_responses(info["resources"]["respondent_id"], info["object_id"])
 
     render plain: "OK"
